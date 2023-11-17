ddb72b3de5541fcb22dcabf168ea9e99ad2ec25d
Rabsterd <ksm6798@gmail.com>
2023-11-17 17:43:01 +0900

    11/17

---
+++ finalProject/src/main/java/edu/kh/project/main/controller/MainController.java
@@ -0,0 +1,5 @@
+package edu.kh.project.main.controller;
+
+public class MainController {
+
+}
---
+++ finalProject/src/test/java/edu/kh/project/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kh.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
