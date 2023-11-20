e0b097f3a0cdef8e074a6958d3b70677e4c080b3
XXXXsonjisung <jklasd9909@gmail.com>
2023-11-20 10:24:02 +0900

    11.20

---
+++ final-project/src/main/java/team/gsk/project/common/config/FilterConfig.java
@@ -0,0 +1,30 @@
+package team.gsk.project.common.config;
+
+import java.util.Arrays;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import team.gsk.project.common.filter.LoginFilter;
+
+@Configuration
+public class FilterConfig {
+	
+	@Bean
+	public FilterRegistrationBean<LoginFilter> loginFilter(){
+		
+		FilterRegistrationBean<LoginFilter> resiRegistrationBean = new FilterRegistrationBean<LoginFilter>();
+		
+		resiRegistrationBean.setFilter(new LoginFilter());
+		
+		String[] url = {"/myPage/*", "/board2/*"};
+		resiRegistrationBean.setUrlPatterns(Arrays.asList(url)); // url 패턴 여러 개 지정
+		resiRegistrationBean.setName("loginFilter"); // 이름
+		resiRegistrationBean.setOrder(1); // 여러 필터가 있을 때 순서
+		return resiRegistrationBean;
+	}
+	
+	
+
+}
