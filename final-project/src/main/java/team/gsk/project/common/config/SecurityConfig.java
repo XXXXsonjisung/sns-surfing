d2d9c50e1d408eefa562c68f07c9fc68e84f96ad
Rabsterd <ksm6798@gmail.com>
2023-11-20 11:20:30 +0900

    security

--- final-project/src/main/java/team/gsk/project/common/config/SecurityConfig.java
+++ final-project/src/main/java/team/gsk/project/common/config/SecurityConfig.java
@@ -2,11 +2,9 @@ package team.gsk.project.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-//@EnableWebSecurity
 public class SecurityConfig {
 	
 	@Bean
