e4b0993cc9ce28d54c8adade06503f7cdd11135f
XXXXsonjisung <jklasd9909@gmail.com>
2023-11-20 12:46:50 +0900

    11.20

--- final-project/src/main/java/team/gsk/project/main/controller/MainController.java
+++ final-project/src/main/java/team/gsk/project/main/controller/MainController.java
@@ -1,20 +1,20 @@
-//package team.gsk.project.main.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//
-//@Controller
-//public class MainController {
-//	
-//	@RequestMapping("/")
-//	public String mainForward(Model model) {
-//		
-//		model.addAttribute("name","ㅋㅋㅋ");
-//	
-//		return "common/main";
-//	}
-//	
-//
-//}
+package team.gsk.project.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/")
+	public String mainForward(Model model) {
+		
+		model.addAttribute("name","ㅋㅋㅋ");
+	
+		return "common/main.html";
+	}
+	
+
+}
