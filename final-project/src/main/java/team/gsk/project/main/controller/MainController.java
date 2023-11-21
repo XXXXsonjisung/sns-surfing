cf5ce5b3af17b33917aafcbcd1e19921e966cb58
Rabsterd <ksm6798@gmail.com>
2023-11-21 17:36:04 +0900

    11/21

--- final-project/src/main/java/team/gsk/project/main/controller/MainController.java
+++ final-project/src/main/java/team/gsk/project/main/controller/MainController.java
@@ -1,33 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-//package team.gsk.project.main.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//
-//@Controller
-//public class MainController {
-//	
-//	@RequestMapping("/")
-//	public String mainForward(Model model) {
-//		
-//		model.addAttribute("name","ㅋㅋㅋ");
-//	
-//		return "common/main";
-//	}
-//
-//
-//}
-=======
-=======
-
->>>>>>> 89180ce51836a194411a075b508ef3f1a8b7337e
 package team.gsk.project.main.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -39,21 +14,27 @@ public class MainController {
 		
 		model.addAttribute("name","ㅋㅋㅋ");
 	
-<<<<<<< HEAD
-		return "common/main.html";
-=======
-		//return "common/main.html";
+
+		return "common/main";
+
 		
 		
-		return "chatting/chatting_choose";
-		
->>>>>>> 89180ce51836a194411a075b508ef3f1a8b7337e
+//		return "chatting/chatting_choose";
+
 	}
 	
+	@GetMapping("/loginPage")
+	public String loginPageForward(Model model) {
+		
+
+		return "common/login";
+
+		
+
+	}
+	
+	
+
 
 }
-<<<<<<< HEAD
->>>>>>> 8c898009e696fd3e7731b56f8c995e1489048695
-=======
 
->>>>>>> 89180ce51836a194411a075b508ef3f1a8b7337e
