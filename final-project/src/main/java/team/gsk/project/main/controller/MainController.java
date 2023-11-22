a7fbcfb5c7c870b9b3fbcec3d28ad50292672b01
eee595398 <eee595398@gmail.com>
2023-11-22 14:48:09 +0900

    1122

--- final-project/src/main/java/team/gsk/project/main/controller/MainController.java
+++ final-project/src/main/java/team/gsk/project/main/controller/MainController.java
@@ -15,10 +15,10 @@ public class MainController {
 		model.addAttribute("name","ㅋㅋㅋ");
 
 		
-		return "common/main";
+		//return "common/main";
 		
 
-//		return "chatting/chatting_choose";
+	return "chatting/chatting_choose";
 
   }
 	
--- final-project/src/main/resources/static/js/chatting/room_making.js
+++ final-project/src/main/resources/static/js/chatting/room_making.js
@@ -1,15 +1,54 @@
-const checkbox = document.getElementById('public');
-const label = document.querySelector('.custom-checkbox');
 
-label.addEventListener('click', function() {
-  if (checkbox.checked) {
-    checkbox.checked = false;
-    label.classList.remove('checked');
-  } else {
-    checkbox.checked = true;
-    label.classList.add('checked');
-  }
+
+// 공개 비공개 
+const checkboxes = document.querySelectorAll('.public');
+const labeles = document.querySelectorAll('.custom-checkbox');
+
+// 비공개 체크시 비밀번호 입력창 뜸
+const roomPw = document.getElementById("roomPw");
+
+labeles.forEach((label,index)=>{
+
+  label.addEventListener('click',()=>{
+
+    const checkbox = checkboxes[index];
+
+    if(!checkbox.checked){
+
+      checkboxes.forEach((cb,i)=>{
+
+          cb.checked=false;
+          labeles[i].classList.remove('checked');
+
+      });
+
+      checkbox.checked = true;
+      label.classList.add('checked');
+
+    }
+
+
+      if(checkboxes[1].checked){
+
+        roomPw.style.display="block";
+
+      }
+
+      if(checkboxes[0].checked){
+
+        roomPw.style.display="none";
+      }
+
+
+  });
+
+
 });
+
+
+
+
+
 
 
 // 태그 리스트 모달창
@@ -58,4 +97,64 @@ function showListItem(index) {
   
   // 특정 인덱스의 list-item 요소만 보이게 하기
   listItems[index].style.display = 'block';
+}
+
+
+
+
+// 태그 선택시 input창에 쌓이게 하기 
+
+const tagsInput = document.getElementById("tagsInput");
+const listItems = document.querySelectorAll(".list-item");
+
+listItems.forEach(item=>{
+
+  item.tagCount =0;
+
+  item.addEventListener('click',()=>{
+
+
+    const tag = item.textContent;
+    const currentTags = tagsInput.value;
+    const tagCount = item.tagCopublicunt;
+    
+
+      //태그 개수 제한 
+      if(tagCount ===5){
+
+          alert("5개만 선택 가능합니다.");
+          return;
+      }   
+      // 태그 추가
+      if (currentTags === '') {
+          tagsInput.value = '#'+tag
+      } else {
+          tagsInput.value = `${currentTags}, ${'#'+tag}`;
+      }
+
+      tagCount++;
+
+  
+
+    });
+
+
+
+});
+
+
+
+
+//프로필 이미지 프리뷰
+
+function image(input) {
+  if (input.files && input.files[0]) {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      document.getElementById('preview').src = e.target.result;
+    };
+    reader.readAsDataURL(input.files[0]);
+  } else {
+    document.getElementById('preview').src = "";
+  }
 }
