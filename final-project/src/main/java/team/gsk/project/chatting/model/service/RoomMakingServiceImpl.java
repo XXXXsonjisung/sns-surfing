5f5542785934ba48d4195dc52a33834487c71550
eee595398 <eee595398@gmail.com>
2023-11-23 17:24:56 +0900

    1123

--- final-project/src/main/java/team/gsk/project/chatting/model/service/RoomMakingServiceImpl.java
+++ final-project/src/main/java/team/gsk/project/chatting/model/service/RoomMakingServiceImpl.java
@@ -4,11 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import team.gsk.project.chatting.model.dao.RoomMakingMapper;
+import team.gsk.project.chatting.model.dto.Chatting;
 
 @Service
 public class RoomMakingServiceImpl implements RoomMakingService {
 
 	@Autowired
 	private RoomMakingMapper mapper;
+
+	@Override
+	public int roomMaking(Chatting inputChatting) {
+	
+		System.out.println(inputChatting);
+		return mapper.roomMaking(inputChatting);
+	}
+
+
 	
 }
