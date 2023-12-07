3de142beb88db06a21f545c9b0e0ddf3a125b108
eee595398 <eee595398@gmail.com>
2023-12-07 19:48:22 +0900

    1207

--- final-project/src/main/java/team/gsk/project/chatting/controller/ChatRoomController.java
+++ final-project/src/main/java/team/gsk/project/chatting/controller/ChatRoomController.java
@@ -3,11 +3,15 @@ package team.gsk.project.chatting.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.HtmlUtils;
 
 import lombok.extern.slf4j.Slf4j;
 import team.gsk.project.chatting.model.dto.ChattingMessage;
@@ -33,6 +37,17 @@ public class ChatRoomController {
 		return messages;
 
 	}
+	
+	
+	   // "/app/chat" 엔드포인트로 메시지가 전송되면 이 메서드가 호출됩니다.
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+    public ChattingMessage sendMessage(@Payload ChattingMessage chatMessage) {
+        // 메시지를 처리하고 반환 (예: HTML 태그 방지)
+        chatMessage.setMessage(HtmlUtils.htmlEscape(chatMessage.getMessage()));
+        return chatMessage;
+    }
+
 	
 
 }
--- final-project/src/main/resources/static/js/chatting/chatting_group.js
+++ final-project/src/main/resources/static/js/chatting/chatting_group.js
@@ -8,37 +8,106 @@ const chatroomList = document.getElementById("room-list");
 const messageArea = document.getElementById('messageArea'); // 채팅 메시지를 표시할 영역
 
 
+// 서버에 연결
+stompClient.connect({}, frame=> {
+
+/*    // 서버로부터 메시지 수신 대기
+    stompClient.subscribe('/topic/roomNo', function(message) {
+        // 메시지 처리
+        displayChat(message.body);
+    });*/
+    
+});
+
+
+
+
 // 채팅방 리스트 눌렀을때 메세지 나오는 함수
 chatroomList.addEventListener('click', function(event) {
-  // 클릭된 요소가 li 요소인지 확인
-  if (event.target.tagName === 'LI') {
+  // 클릭된 요소 또는 그 부모 요소 중 LI 요소 찾기
+  let li = event.target.closest('li');
+  
+  // LI 요소가 클릭되었는지 확인
+  if (li) {
     // 선택한 채팅방의 데이터 속성 값 가져오기
-    const roomNo = event.target.getAttribute('room-no');
+    const roomNo = li.getAttribute('room-no');
     
-    
- 	 fetch('/getOldMessage?roomNo=' + roomNo)
-     .then(response => response.text())
-	 .then( messages => {
-                // 채팅 내역을 받아와서 화면에 표시하기
-                console.log(messages);
-                displayChat(messages);
-            
-            })
-            .catch(error => {
-                console.error('에러 :', error);
-            });
-    
-    
-    
+    fetch('/getOldMessage?roomNo=' + roomNo)
+      .then(response => response.text())
+      .then(messages => {
+        // 채팅 내역을 받아와서 화면에 표시하기
+        console.log(messages);
+        displayChat(messages);
+      })
+      .catch(error => {
+        console.error('에러 :', error);
+      });
     
     console.log('방번호 :', roomNo);
   }
 });
 
-// 채팅방 메세지 보여주는 함수
-function displayChat(messages){
-	
-  	messageArea.innerHTML = messages; // 이전 채팅 내용을 표시
 
-	
+// 메시지를 화면에 표시하는 함수
+function displayChat(messages) {
+    // messageArea 초기화
+    messageArea.innerHTML = '';
+
+    // messages를 JSON 객체로 변환
+    const parsedMessages = JSON.parse(messages);
+
+    // 각 메시지를 순회하면서 화면에 추가
+    parsedMessages.forEach(message => {
+        const messageElement = document.createElement('li');
+        messageElement.className = 'target-chat';
+
+        // 메시지 내용을 구성
+        messageElement.innerHTML = `
+            <img id="pro" src="images/${message.sender}.jpg">
+            <div>
+                <b>${message.sender}</b><br>
+                <p class="chat">${message.message}</p>
+                <span class="chatDate">${message.time}</span>
+            </div>
+        `;
+
+        // messageArea에 메시지 요소 추가
+        messageArea.appendChild(messageElement);
+    });
 }
+
+
+// 메시지 전송 함수
+function sendMessage() {
+    const messageInput = document.querySelector('form input[type="text"]');
+    const message = messageInput.value;
+
+    // 입력된 메시지가 비어있지 않은 경우에만 전송
+    if (message.trim() !== '') {
+        // 메시지 객체 생성 (서버에서 요구하는 형식에 맞춰 수정 필요)
+        const chatMessage = {
+            type: 'TALK', // 예시로 'TALK' 설정, 실제 사용에 맞게 조정 필요
+            // roomNo, sender 등 다른 필요한 정보 추가
+            message: message,
+            // time: 현재 시간 설정 (서버에서 처리할 수도 있음)
+        };
+
+        // WebSocket을 통해 서버에 메시지 전송
+        stompClient.send("/app/chat", {}, JSON.stringify(chatMessage));
+
+        // 입력 필드 초기화
+        messageInput.value = '';
+    }
+}
+
+// 전송 버튼 이벤트 리스너
+document.getElementById('submit').addEventListener('click', sendMessage);
+
+// 폼에서 엔터 키 누르면 메시지 전송 (선택 사항)
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    sendMessage();
+});
+
+
+
