4bda193348c35d012bd24d4764cd665cd4593377
Rabsterd <ksm6798@gmail.com>
2023-12-18 17:47:16 +0900

    12/18

--- final-project/src/main/resources/static/js/common/login.js
+++ final-project/src/main/resources/static/js/common/login.js
@@ -1,4 +1,3 @@
-
 const loginFrm = document.getElementById("loginFrm");
 
 const memberId = document.querySelector("#loginFrm input[name='memberId']");
@@ -35,6 +34,38 @@ if(loginFrm != null) {
 
     });
 }
+
+
+  function loginWithKakao() {
+    Kakao.Auth.authorize({
+      redirectUri: 'https://localhost',
+    });
+  }
+
+  // 아래는 데모를 위한 UI 코드입니다.
+  displayToken()
+  function displayToken() {
+    var token = getCookie('authorize-access-token');
+
+    if(token) {
+      Kakao.Auth.setAccessToken(token);
+      Kakao.Auth.getStatusInfo()
+        .then(function(res) {
+          if (res.status === 'connected') {
+            document.getElementById('token-result').innerText
+              = 'login success, token: ' + Kakao.Auth.getAccessToken();
+          }
+        })
+        .catch(function(err) {
+          Kakao.Auth.setAccessToken(null);
+        });
+    }
+  }
+
+  function getCookie(name) {
+    var parts = document.cookie.split(name + '=');
+    if (parts.length === 2) { return parts[1].split(';')[0]; }
+  }
 
 
 
