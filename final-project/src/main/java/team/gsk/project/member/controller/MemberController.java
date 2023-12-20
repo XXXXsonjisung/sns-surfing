ae318e10fb9fa93e82d49c0c0b3c417ef681fc6d
Rabsterd <ksm6798@gmail.com>
2023-12-20 17:40:15 +0900

    12/20

--- final-project/src/main/java/team/gsk/project/member/controller/MemberController.java
+++ final-project/src/main/java/team/gsk/project/member/controller/MemberController.java
@@ -3,28 +3,22 @@ package team.gsk.project.member.controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import oracle.jdbc.OracleDatabaseException;
 import team.gsk.project.member.model.dto.Member;
 import team.gsk.project.member.model.service.MemberService;
 
@@ -68,6 +62,16 @@ public class MemberController {
 		
 		return "redirect:/";
 	}
+	
+	@ResponseBody
+	@GetMapping("/kakao")
+	public String  kakaoCallback(@RequestParam String code) throws OracleDatabaseException {
+
+	            System.out.println(code);
+	            
+	            return "redirect:/";
+
+	    }
 	
 	// 아이디/비밀번호 찾기 페이지 이동
 	@GetMapping("/searchIdPw")
--- final-project/src/main/java/team/gsk/project/member/model/service/APILoginServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package team.gsk.project.member.model.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class APILoginServiceImpl implements APILoginService{
-
-}
